44d30a7bfb73325ef5a19c582b328da1cd74fe80
Chris Suran <christopher.suran@harness.io>
2022-11-17 15:54:19 -0800

    Fix FF Code

--- src/main/java/io/harness/springforward/AboutController.java
+++ src/main/java/io/harness/springforward/AboutController.java
@@ -9,25 +9,25 @@ import io.harness.cf.client.dto.Target;
 @RestController
 public class AboutController {
 
-	/**
-	* Put the API Key here from your environment
-	*/
-	String apiKey = '35adfc78-ea47-4290-a51d-9515f4c7e930';
-
-	CfClient cfClient = new CfClient(apiKey, Config.builder().build());
-
-	/**
-	* Define you target on which you would like to evaluate
-	* the featureFlag
-	*/
-	Target target = Target.builder()
-						.name("User1")
-						.attributes(new HashMap<String, Object>())
-						.identifier("user1@example.com")
-						.build();
-
 	@GetMapping("/about")
 	public String index() {
+		/**
+		* Put the API Key here from your environment
+		*/
+		String apiKey = '35adfc78-ea47-4290-a51d-9515f4c7e930';
+
+		CfClient cfClient = new CfClient(apiKey, Config.builder().build());
+
+		/**
+		* Define you target on which you would like to evaluate
+		* the featureFlag
+		*/
+		Target target = Target.builder()
+							.name("User1")
+							.attributes(new HashMap<String, Object>())
+							.identifier("user1@example.com")
+							.build();
+
 		if cfClient.boolVariation("ABOUT_PAGE", target, false) {
 			return "FF is on!";
 		} else {
