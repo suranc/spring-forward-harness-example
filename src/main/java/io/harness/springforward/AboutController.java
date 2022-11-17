6b92b22c2db51d97bbc76c579f07386bb9a59c7c
Chris Suran <christopher.suran@harness.io>
2022-11-17 15:35:25 -0800

    Fix FF Code

--- src/main/java/io/harness/springforward/AboutController.java
+++ src/main/java/io/harness/springforward/AboutController.java
@@ -3,25 +3,28 @@ package io.harness.springforward;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
-* Put the API Key here from your environment
-*/
-String apiKey = '35adfc78-ea47-4290-a51d-9515f4c7e930';
-
-CfClient cfClient = new CfClient(apiKey, Config.builder().build());
-
-/**
-* Define you target on which you would like to evaluate
-* the featureFlag
-*/
-Target target = Target.builder()
-                    .name("User1")
-                    .attributes(new HashMap<String, Object>())
-                    .identifier("user1@example.com")
-                    .build();
+import io.harness.cf.client.api.*;
+import io.harness.cf.client.dto.Target;
 
 @RestController
 public class AboutController {
+
+	/**
+	* Put the API Key here from your environment
+	*/
+	String apiKey = '35adfc78-ea47-4290-a51d-9515f4c7e930';
+
+	CfClient cfClient = new CfClient(apiKey, Config.builder().build());
+
+	/**
+	* Define you target on which you would like to evaluate
+	* the featureFlag
+	*/
+	Target target = Target.builder()
+						.name("User1")
+						.attributes(new HashMap<String, Object>())
+						.identifier("user1@example.com")
+						.build();
 
 	@GetMapping("/about")
 	public String index() {
