c2e3e3a3f6c83944922bdf9f15d816dba84fcc15
Chris Suran <christopher.suran@harness.io>
2022-12-29 13:15:55 -0500

    Add default string for FF api key

--- src/main/java/io/harness/springforward/AboutController.java
+++ src/main/java/io/harness/springforward/AboutController.java
@@ -9,7 +9,7 @@ import io.harness.cf.client.dto.Target;
 @RestController
 public class AboutController {
 
-	private final String apiKey = (System.getenv("FF_API_KEY") != null) ? System.getenv("FF_API_KEY") : "";
+	private final String apiKey = (System.getenv("FF_API_KEY") != null) ? System.getenv("FF_API_KEY") : "no-api-key";
 	private CfClient cfClient = new CfClient(apiKey);
 
 	@GetMapping("/about")
