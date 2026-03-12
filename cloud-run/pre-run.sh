beb62e9313d72fe900746006cfba52d30014134a
Chris Suran <christopher.suran@harness.io>
2026-03-11 21:38:49 -0400

    Adding output

--- cloud-run/pre-run.sh
+++ cloud-run/pre-run.sh
@@ -1,3 +1,5 @@
 export PLUGIN_CLOUD_RUN_SERVICE_MANIFEST_FILE_CONTENT=$(echo $PLUGIN_CLOUD_RUN_SERVICE_MANIFEST_FILE_CONTENT|sed 's/value/newvalue/')
+echo "Running Prerun"
+echo $PLUGIN_CLOUD_RUN_SERVICE_MANIFEST_FILE_CONTENT
 /opt/harness/bin/harness-cloud-run-plugin
 exit 0
